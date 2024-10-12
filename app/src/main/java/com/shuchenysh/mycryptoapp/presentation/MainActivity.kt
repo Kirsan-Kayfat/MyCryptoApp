457b970df13cdde1c6fe82ad4b5793eeaba897e6
Mamed Khasiev <shuchenysh@gmail.com>
2024-10-12 14:56:21 +0300

    Added landscape mode for MainActivity.kt

--- app/src/main/java/com/shuchenysh/mycryptoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/shuchenysh/mycryptoapp/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.shuchenysh.mycryptoapp.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.shuchenysh.mycryptoapp.R
 import com.shuchenysh.mycryptoapp.databinding.ActivityMainBinding
 import com.shuchenysh.mycryptoapp.domain.CoinInfo
 import com.shuchenysh.mycryptoapp.presentation.adapters.CoinInfoAdapter
@@ -21,16 +22,35 @@ class MainActivity : AppCompatActivity() {
         val adapter = CoinInfoAdapter(this)
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinInfo) {
-                val intent = CoinDetailActivity.newIntent(
-                    this@MainActivity,
-                    coinPriceInfo.fromSymbol
-                )
-                startActivity(intent)
+                if (isOnePaneMode()) {
+                    launchDetailActivity(coinPriceInfo.fromSymbol)
+                } else {
+                    launchDetailFragment(coinPriceInfo.fromSymbol)
+                }
             }
         }
-        binding.rvCoinPriceList?.adapter = adapter
-        binding.rvCoinPriceList?.itemAnimator = null
+        binding.rvCoinListContainer.adapter = adapter
+        binding.rvCoinListContainer.itemAnimator = null
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.coinInfoList.observe(this) { adapter.submitList(it) }
+    }
+
+    private fun isOnePaneMode() = binding.mainFragmentContainer == null
+
+    private fun launchDetailActivity(fromSymbol: String) {
+        val intent = CoinDetailActivity.newIntent(
+            this@MainActivity,
+            fromSymbol
+        )
+        startActivity(intent)
+    }
+
+    private fun launchDetailFragment(fromSymbol: String) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+            .addToBackStack(null)
+            .commit()
     }
 }
